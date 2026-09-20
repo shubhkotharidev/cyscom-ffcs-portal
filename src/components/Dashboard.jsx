1c0e3953e23fe3078148170543b90110997a0c95
Shubh Kothari <shubhkothari945@gmail.com>
2026-09-20 17:22:45 +0530

    style: change dashboard name font to Revalia

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -57,7 +57,7 @@ export default function Dashboard({ user, setTab, onSubmitContribution, pending 
     <div className="cg-fade-in">
       <div style={{ position: "relative", marginBottom: 32 }}>
         <div className="cg-label" style={{ position: "relative", zIndex: 10 }}>WELCOME BACK</div>
-        <div className="cg-display" style={{ fontFamily: "'Silkscreen', cursive", fontSize: 48, fontWeight: 400, margin: "6px 0", letterSpacing: "-0.02em", position: "relative", zIndex: 10 }}>
+        <div className="cg-display" style={{ fontFamily: "'Revalia', cursive", fontSize: 48, fontWeight: 400, margin: "6px 0", letterSpacing: "-0.02em", position: "relative", zIndex: 10 }}>
           {user.name}
         </div>
       </div>
