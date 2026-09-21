f58367f9604d451ea632ab30a73b9548885a304b
Shubh Kothari <shubhkothari945@gmail.com>
2026-09-22 00:50:09 +0530

    revert: undo last 7 dyson sphere commits

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useRef } from "react";
 import gsap from "gsap";
 import { BRAND, SUBTITLE } from "../lib/constants";
-import DysonSphere from "./DysonSphere";
 
 /* ---------------------------------------------------------------------- */
 /* Brutalist B&W static wipe transition                                   */
@@ -67,16 +66,13 @@ export default function Landing({ onEnter }) {
 
         <div className="pixel-pc-setup" style={{ transform: "scale(1.4)", transformOrigin: "top center", marginTop: 20 }}>
           <div className="pixel-pc">
-            <div className="pixel-pc-screen" style={{ flexDirection: "column", justifyContent: "space-evenly", alignItems: "center", padding: "10px 8px" }}>
-              <div className="pixel-pc-text" style={{ alignSelf: "flex-start" }}>C:\&gt;_</div>
-
-              <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <DysonSphere />
-              </div>
-
-              <button className="pixel-pc-enter-btn" onClick={onEnter} style={{ width: "100%" }}>
+            <div className="pixel-pc-screen" style={{ flexDirection: "column", justifyContent: "space-between" }}>
+              <div className="pixel-pc-text">C:\&gt;_</div>
+              
+              <button className="pixel-pc-enter-btn" onClick={onEnter}>
                 [ENTER_PORTAL]
               </button>
+
             </div>
             <div className="pixel-pc-drive"></div>
             <div className="pixel-pc-drive-btn"></div>
